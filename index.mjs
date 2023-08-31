1d704637439b2fe1e84c9c4fb4db85260b522c82
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-08-31 14:37:25 -0300

    Voluntad

--- index.mjs
+++ index.mjs
@@ -1,23 +1,38 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import dns from "dns";
 import * as url from "url";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({ optionsSuccessStatus: 200 }));
-
+const options = {
+  // Setting family as 6 i.e. IPv6
+  family: 6,
+  hints: dns.ADDRCONFIG | dns.V4MAPPED,
+};
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 //app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-// app.get('/api/hello', function(req, res) {
-//   res.json({ greeting: 'hello API' });
-// });
+app.post("/api/shorturl", function (req, res) {
+  const ipAddres = req.body.url;
+  console.log(ipAddres);
+  dns.lookup(
+    ipAddres,
+    options,
+    (err, address, family) => err && res.json({ error: "invalid url" })
+  );
+  res.send({ original_url: ipAddres, short_url: 1 });
+});
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
