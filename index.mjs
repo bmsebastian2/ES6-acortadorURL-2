4e0968e40ac963ab634caaf28c100a3e2b5f833a
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-09-01 12:32:05 -0300

    voluntad 4

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import dns from "dns";
+
 import * as url from "url";
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,7 +14,11 @@ const options = {
   all: true,
 };
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-
+let objectShort = {
+  2: {
+    url: "hhhtp:akslskdlks",
+  },
+};
 //app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.static("public"));
 
@@ -26,14 +31,37 @@ app.get("/", function (req, res) {
 
 app.post("/api/shorturl", function (req, res) {
   const ipAddres = req.body.url;
-  console.log(ipAddres);
+  const shorUrl = getRandomInt(99);
+  // objectShort.id = ipAddres;
+  // objectShort[ipAddres].url = shorUrl;
+  //console.log(shorUrl);
+  //console.log(objectShort);
+  //console.log(objectShort["2"]);
+  let object = { [`${shorUrl}`]: { url: ipAddres } };
+  objectShort = object;
+  //console.log(objectShort);
+
+  //console.log(ipAddres);
   dns.lookup(ipAddres, options, (err, address, family) =>
     res.json(
-      err ? { error: "invalid url" } : { original_url: ipAddres, short_url: 1 }
+      err
+        ? { error: "invalid url" }
+        : { original_url: ipAddres, short_url: shorUrl }
     )
   );
+});
+app.get("/api/shorturl/:short", function (req, res) {
+  const { short } = req.params;
+  console.log(short);
+  console.log(objectShort);
+  console.log(objectShort[short].url);
+  res.redirect(objectShort[short].url);
 });
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
